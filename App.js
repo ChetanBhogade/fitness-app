fef380cf86fbf18c39040925afa475b827ac89ae
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-07-23 22:00:29 +0530

    adding timer screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import TimerScreen from './screens/TimerScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator headerMode="none">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Timer" component={TimerScreen} />
       </Stack.Navigator>
       <StatusBar hidden />
     </NavigationContainer>
---
+++ screens/TimerScreen.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import MyHeader from "../components/MyHeader";
+import { getFlexiblePixels } from "../MyUtils";
+import { useFonts } from "expo-font";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+
+function TimerScreen() {
+  let [fontsLoaded] = useFonts({
+    Pacifico: require("../assets/Fonts/Pacifico/Pacifico-Regular.ttf"),
+  });
+
+  const [timerNumber, setTimerNumber] = useState(3);
+
+  useEffect(() => {
+    var intervalNo;
+    if (timerNumber > 0) {
+      intervalNo = setInterval(() => {
+        setTimerNumber(timerNumber - 1);
+      }, 1000);
+    }
+
+    if (timerNumber === 0) {
+      console.log("timer reaches.......");
+      // replacing window code goes here
+    }
+
+    return () => {
+      clearInterval(intervalNo);
+    };
+  }, [setInterval, timerNumber]);
+
+  return (
+    <View>
+      <View style={styles.rectangle}>
+        {fontsLoaded ? (
+          <Text style={styles.caption}>Starting...</Text>
+        ) : (
+          <Text>Loading</Text>
+        )}
+      </View>
+      <View style={styles.timer}>
+        <View style={styles.eclipse}>
+          {fontsLoaded ? (
+            <Text style={styles.timerNumber}>{timerNumber}</Text>
+          ) : (
+            <Text>Loading</Text>
+          )}
+        </View>
+      </View>
+      <MyHeader />
+    </View>
+  );
+}
+
+const boxWidth = getFlexiblePixels((pixels = 388), (isWidth = true));
+const boxHeight = getFlexiblePixels((pixels = 365), (isWidth = false));
+
+const styles = StyleSheet.create({
+  main: {
+    position: "relative",
+    backgroundColor: "#FFFFFF",
+  },
+  timer: {
+    position: "absolute",
+    width: boxWidth,
+    height: boxHeight,
+    left: (SCREEN_WIDTH - boxWidth) / 2,
+    top: (SCREEN_HEIGHT - boxHeight) / 3,
+    alignItems: "center",
+  },
+  eclipse: {
+    position: "absolute",
+    width: 280,
+    height: 280,
+    borderWidth: 20,
+    borderStyle: "solid",
+    borderColor: "#6E7BF0",
+    borderRadius: 280 / 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  timerNumber: {
+    width: 280,
+    height: 280,
+    fontFamily: "Pacifico",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: 150,
+    lineHeight: 250,
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+    color: "#000000",
+  },
+  rectangle: {
+    position: "absolute",
+    width: getFlexiblePixels((pixels = 411), (isWidth = true)),
+    height: getFlexiblePixels((pixels = 75), (isWidth = false)),
+    left: 0,
+    top: SCREEN_HEIGHT - getFlexiblePixels((pixels = 262), (isWidth = false)),
+    backgroundColor: "rgba(219, 78, 78, 1)",
+  },
+  caption: {
+    position: "absolute",
+    borderWidth: 1,
+    width: getFlexiblePixels((pixels = 411), (isWidth = true)),
+    height: getFlexiblePixels((pixels = 75), (isWidth = false)),
+    fontFamily: "Pacifico",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: 35,
+    lineHeight: 60,
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+    color: "#000000",
+  },
+});
+
+export default TimerScreen;
